b4b5e26313158e1b16f99d12389569d904b952a0
nyusporttkd <139005055+hjk665@users.noreply.github.com>
2024-10-10 16:23:42 -0400

    Fixed minor errors in test.py

--- data/people.py
+++ data/people.py
@@ -1,6 +1,4 @@
-"""
-This module interfaces to a data layer that endpoints make use of.
-"""
+# people.py
 
 NAME = "name"
 ROLES = "roles"
@@ -25,71 +23,29 @@ TEST_PERSON_DICT = {
     },
 }
 
-
 def create_person(name: str, affiliation: str, email: str):
-    """
-    Creates a new entity Person.
-    Returns new Person with fields: name, affiliation, email.
-    Args:
-        string: name, affiliation, email_address
-    Returns:
-        None
-    """
     if email in TEST_PERSON_DICT:
         raise ValueError(f'Trying to add duplicate: {email=}')
-    TEST_PERSON_DICT[email] = {NAME: name, AFFILIATION: affiliation, 
-                               EMAIL: email}
+    TEST_PERSON_DICT[email] = {NAME: name, AFFILIATION: affiliation, EMAIL: email}
+    return TEST_PERSON_DICT[email]  # Return the created entry
 
+def update_person(email: str, name=None, affiliation=None):
+    if email not in TEST_PERSON_DICT:
+        return False
+    if name:
+        TEST_PERSON_DICT[email][NAME] = name
+    if affiliation:
+        TEST_PERSON_DICT[email][AFFILIATION] = affiliation
+    return TEST_PERSON_DICT[email]
 
-def update_people():
-    """
-    Updates given entity Person:
-    Returns given entity with updated fields.
-    Args:
-        Person: person to be updated
-        String: field to be updated
-    Returns:
-        Person: person that was updated
-        Bool(?): maybe return a bool if person was not found or not updated?
-    """
-    pass
+def read_person(email: str):
+    return TEST_PERSON_DICT.get(email)
 
-
-def read_people():
-    """
-    (btw this was the one i was least sure about, so pls edit if it's wrong)
-    Reads information from Person fields and returns it
-    Args:
-        Person: entity you're reading information from
-        String: field you want to read
-    Returns:
-        String: information from field you want to read
-    """
-    pass
-
-
-def delete_people(_id):
-    """
-    Deletes given entity Person.
-    Returns true if successfully deleted, and false if an error occurred.
-
-    Args:
-        Person: person id to be deleted
-    Returns:
-        Bool: true if deleted, false if not.
-    """
-    pass
-
+def delete_person(email: str):
+    if email in TEST_PERSON_DICT:
+        del TEST_PERSON_DICT[email]
+        return True
+    return False
 
 def get_people():
-    """
-    Takes in no arguments but returns a dictionary of users in which
-    each user email must be the key for another dictionary.
-
-    Args:
-        None
-    Returns:
-        Dict: dictionary of users on user email
-    """
-    people = TEST_PERSON_DICT
-    return people
+    return TEST_PERSON_DICT
