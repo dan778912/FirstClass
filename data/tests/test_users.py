fc1e3abd4d11c54fec6e9b9b8d0b489b47c5905e
seha kim <125930924+epochsummer@users.noreply.github.com>
2024-10-10 23:48:20 -0400

    Update test_users.py

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -17,17 +17,3 @@ def test_get_users():
         assert isinstance(user[usrs.LEVEL], int)
 
 
-def test_create_user():
-    users = usrs.get_users()
-    assert NEW_USER_EMAIL not in users
-    new_user = usrs.create_user("New User", NEW_USER_EMAIL, 1)
-    assert new_user is not None
-    users = usrs.get_users()
-    assert NEW_USER_EMAIL in users
-    assert new_user == users[NEW_USER_EMAIL]
-
-def test_delete_user():
-    usrs.create_user("Delete User", "deleteuser@nyu.edu", 1)
-    assert usrs.delete_user("deleteuser@nyu.edu")
-    assert "deleteuser@nyu.edu" not in usrs.get_users()
-
