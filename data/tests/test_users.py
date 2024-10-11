c96536dc3c3da614191319483d4d5f3a4896306d
seha kim <125930924+epochsummer@users.noreply.github.com>
2024-10-10 23:31:49 -0400

    Update test_users.py

--- data/tests/test_users.py
+++ data/tests/test_users.py
@@ -20,4 +20,9 @@ def test_create_user():
     users = usrs.get_users()
     assert NEW_USER_EMAIL in users
     assert new_user == users[NEW_USER_EMAIL]
+    
+def test_delete_user():
+    usrs.create_user("Delete User", "deleteuser@nyu.edu", 1)
+    assert usrs.delete_user("deleteuser@nyu.edu")
+    assert usrs.read_user("deleteuser@nyu.edu") is None
 
