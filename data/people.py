12e20a00304d5aa8de258294fa17c996dadce703
nyusporttkd <139005055+hjk665@users.noreply.github.com>
2024-10-17 21:56:35 -0400

    Flake8 fixed

--- data/people.py
+++ data/people.py
@@ -8,18 +8,19 @@ EMAIL = "email"
 TEST_EMAIL = "aim9061@nyu.edu"
 PROF_TEST_EMAIL = "ejc369@nyu.edu"
 
+
 TEST_PERSON_DICT = {
     TEST_EMAIL: {
         NAME: "Alex Martin",
         ROLES: [],
         AFFILIATION: "NYU",
-        EMAIL: TEST_EMAIL
+        EMAIL: TEST_EMAIL,
     },
     PROF_TEST_EMAIL: {
         NAME: "Eugene Callahan",
         ROLES: [],
         AFFILIATION: "NYU",
-        EMAIL: PROF_TEST_EMAIL
+        EMAIL: PROF_TEST_EMAIL,
     },
 }
 
@@ -34,29 +35,27 @@ def create(name: str, affiliation: str, email: str):
         string: email value in dictionary
     """
     if email in TEST_PERSON_DICT:
-        raise ValueError(f'Trying to add duplicate: {email=}')
+        raise ValueError(f"Trying to add duplicate: {email=}")
     TEST_PERSON_DICT[email] = {
         NAME: name,
         AFFILIATION: affiliation,
-        EMAIL: email
+        EMAIL: email,
     }
     return TEST_PERSON_DICT[email]
 
 
 def update(email: str, name=None, affiliation=None, new_email=None):
     """
-    Updates given entity Person:
-    Can update name, affiliation, or email.
-    
+    Updates a Person's name, affiliation, or email.
+
     Args:
-        email (str): The current email of the person to update.
-        name (str, optional): New name to update. Defaults to None.
-        affiliation (str, optional): New affiliation to update. Defaults to None.
-        new_email (str, optional): New email to update. Defaults to None.
-    
+        email (str): Current email of the person.
+        name (str, optional): New name to update.
+        affiliation (str, optional): New affiliation to update.
+        new_email (str, optional): New email to update.
+
     Returns:
-        dict: The updated person dictionary if successful.
-        Bool: False if the email does not exist in TEST_PERSON_DICT.
+        dict: Updated person dictionary, or False if email not found.
     """
     if email not in TEST_PERSON_DICT:
         return False
@@ -68,12 +67,11 @@ def update(email: str, name=None, affiliation=None, new_email=None):
     if affiliation:
         person[AFFILIATION] = affiliation
     if new_email and new_email != email:
-        # Split the line to conform to PEP 8 standards (79 characters limit)
+        person[EMAIL] = new_email
         TEST_PERSON_DICT[new_email] = person
         del TEST_PERSON_DICT[email]
 
     return person
-
 
 
 def read(email: str):
