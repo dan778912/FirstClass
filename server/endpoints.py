d0882fe208bef2aee499a482cd3491c13a4cf4cb
seha kim <125930924+epochsummer@users.noreply.github.com>
2024-10-19 23:00:59 -0400

    endpoints.py

--- server/endpoints.py
+++ server/endpoints.py
@@ -43,7 +43,10 @@ class HelloWorld(Resource):
         A trivial endpoint to see if the server is running.
         It just answers with "hello world."
         """
-        return {HELLO_RESP: 'world'}
+    try:
+        return ppl.get()
+    except Exception as e:
+        raise wz.InternalServerError(f"Error fetching people: {e}")
 
 
 @api.route('/endpoints')
