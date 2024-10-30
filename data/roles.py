098c5bed5fcb2bdb2da065727f95990ba88ceb79
dan778912 <ayy9673@nyu.edu>
2024-10-31 03:48:14 +0900

    Add roles functions

--- data/roles.py
+++ data/roles.py
@@ -23,12 +23,24 @@ def get_roles() -> dict:
     return ROLES
 
 
+def get_masthead_roles() -> dict:
+    mh_roles = get_roles()
+    del_mh_roles = []
+    for role in mh_roles:
+        if role not in MH_ROLES:
+            del_mh_roles.append(role)
+    for del_role in del_mh_roles:
+        del mh_roles[del_role]
+    return mh_roles
+
+
 def is_valid(code: str) -> bool:
     return code in ROLES
 
 
 def main():
     print(get_roles())
+    print(get_masthead_roles())
 
 
 if __name__ == '__main__':
