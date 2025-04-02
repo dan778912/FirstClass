17b4cec3b7ad87d133aea1b70a9a53d21cde9284
dauphineezc <124481595+dauphineezc@users.noreply.github.com>
2025-04-01 23:50:53 -0400

    endpoint to send state transitions to front end

--- server/endpoints.py
+++ server/endpoints.py
@@ -398,6 +398,16 @@ class CreateManuscript(Resource):
             raise wz.NotAcceptable(f'Failed to create manuscript: {str(err)}')
 
 
+@api.route(f'{MANU_EP}/state_transitions')
+class ManuscriptStateTransitions(Resource):
+    def get(self):
+        transitions = {}
+        for state in manu.VALID_STATES:
+            # Convert the set of valid actions into a list for JSON serialization.
+            transitions[state] = list(manu.get_valid_actions_by_state(state))
+        return transitions
+
+
 AUTH_FLDS = api.model('AuthenticateUser', {
     ppl.EMAIL: fields.String(required=True),
     ppl.PASSWORD: fields.String(required=True)
